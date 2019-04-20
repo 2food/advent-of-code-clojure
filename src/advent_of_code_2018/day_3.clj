b4e3f669b03ee4a25c5390da64a3b352d3b81300
Tormod Mathiesen <tormathi@gmail.com>
2019-04-19 22:45:47 -0400

    day 3 - part 1

--- src/advent_of_code_2018/day_3.clj
+++ src/advent_of_code_2018/day_3.clj
@@ -19,3 +19,20 @@
      :top  (read-string top-s)
      :width (read-string width-s)
      :height (read-string height-s)}))
+
+(defn calc-coords
+  [{:keys [left top width height] :as claim}]
+  (let [coords (for [x (range left (+ left width))
+                     y (range top (+ top height))]
+                 [x y])]
+    (assoc claim :coords coords)))
+
+(defn count-covers
+  [claims]
+  (reduce (fn [acc {cs :coords}] (merge-with + acc (zipmap cs (repeat 1))))
+          {}
+          claims))
+
+(defn santa-cloth-claims [] (count-covers (map (comp calc-coords read-claim) (input))))
+
+(defn answer-1 [] (count (filter (fn [[k v]] (when (> v 1) k)) (santa-cloth-claims))))
