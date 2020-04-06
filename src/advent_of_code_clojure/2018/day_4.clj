f4b9e371500755916c9dddddaf0cadfbd12aa5f9
Tormod Mathiesen <tormathi@gmail.com>
2020-04-06 08:18:39 +0200

    4

--- src/advent_of_code_clojure/2018/day_4.clj
+++ src/advent_of_code_clojure/2018/day_4.clj
@@ -1,7 +1,9 @@
 (ns advent-of-code-clojure.2018.day-4
   (:require [advent-of-code-clojure.inputs :as inputs]
             [clojure.string :as string]
-            [clj-time.format :as f]))
+            [clj-time.format :as f]
+            [clj-time.core :as t]
+            [clojure.math.numeric-tower :as math]))
 
 (defn markings [] (sort (inputs/lines (inputs/get-input-for-day 2018 4))))
 
@@ -24,7 +26,7 @@
                  (rest remaining-markings)))))))
 
 (comment
-  (map println (group-shifts (markings))))
+  (group-shifts (markings)))
 
 (def example-shift-strs ["[1518-11-13 00:00] Guard #1877 begins shift" "[1518-11-13 00:10] falls asleep" "[1518-11-13 00:43] wakes up"])
 (def example-event-str (first example-shift-strs))
@@ -36,7 +38,7 @@
 (defn str->action
   [s]
   (cond
-    (re-find #"begins" s) {:guard (Long/parseLong (last (re-matches #".*#(\d+) .*" s)))
+    (re-find #"begins" s) {:guard  (Long/parseLong (last (re-matches #".*#(\d+) .*" s)))
                            :action :starts}
     (re-find #"wakes" s) {:action :wakes}
     (re-find #"falls asleep" s) {:action :falls-asleep}))
@@ -64,3 +66,61 @@
                   shifts-str-groups->shifts)
               3251)))
 
+(defn merge-min-vecs [min-vecs]
+  (mapv #(reduce + %) (partition (count min-vecs) (apply interleave min-vecs))))
+
+(defn min-vec' [r prev-min [f & rest]]
+  (if (some? f)
+    (let [now (t/minute (:timestamp f))]
+      (into (vec (repeat (- now prev-min) r))
+            (min-vec' (math/abs (- r 1)) now rest)))
+    []))
+
+(defn pad-to-60 [v]
+  (into (vec v) (repeat (- 60 (count v)) 0)))
+
+(defn min-vec [v]
+  (pad-to-60 (min-vec' 0 0 (rest v))))
+
+(defn shifts->min-vectors [shiftmap]
+  (reduce-kv (fn [acc k v]
+               (into acc {k (merge-min-vecs (map min-vec v))}))
+             {}
+             shiftmap))
+
+(defn guard-sleep-times []
+  (-> (markings)
+      group-shifts
+      shifts-str-groups->shifts
+      shifts->min-vectors))
+
+(defn most-asleep []
+  (reduce-kv (fn [[oldk oldv] k v]
+               (let [newv (reduce + v)]
+                 (if (< oldv newv)
+                   [k newv]
+                   [oldk oldv])))
+             [0 0]
+             (guard-sleep-times)))
+
+(def correct-1 151754)
+(defn answer-1 []
+  (let [[guard _] (most-asleep)
+        sleep-mins ((guard-sleep-times) guard)]
+    (* guard (.indexOf sleep-mins (apply max sleep-mins)))))
+
+(defn highest-consistent-sleep []
+  (reduce-kv (fn [[oldk oldv] newk newv]
+               (let [old (apply max oldv)
+                     new (apply max newv)]
+                 (if (< old new)
+                   [newk newv]
+                   [oldk oldv])))
+             [0 [0]]
+             (guard-sleep-times)))
+
+; wrong 12435, too low
+(def correct-2 19896)
+(defn answer-2 []
+  (let [[guard sleep-mins] (highest-consistent-sleep)]
+    (* guard (.indexOf sleep-mins (apply max sleep-mins)))))
