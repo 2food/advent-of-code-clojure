1f1975b084be82478a41bd474cbac9bbbb83830f
Tormod Mathiesen <tormathi@gmail.com>
2021-12-01 09:06:26 +0100

    day 1 part 1

--- src/advent_of_code_clojure/2021/day_1.clj
+++ src/advent_of_code_clojure/2021/day_1.clj
@@ -2,3 +2,18 @@
   (:require [advent-of-code-clojure.inputs :as inputs]))
 
 (defn input [] (inputs/read-longs (inputs/get-input-for-day 2021 1)))
+
+(defn greater-than-prev? [nums]
+  (let [rest-nums (rest nums)]
+    (map < nums rest-nums)))
+
+(comment
+  (= (greater-than-prev? [1 2 2 4 1]) [true false true false]))
+
+(defn n-greater-than-prev [nums]
+  (count (filter identity (greater-than-prev? nums))))
+
+(comment
+  (n-greater-than-prev (input)))
+
+
