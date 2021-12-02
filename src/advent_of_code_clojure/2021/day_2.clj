4e5cab2c3ed4ba276d810c4e1e30c0d4ea7ac456
Tormod Mathiesen <tormathi@gmail.com>
2021-12-02 09:37:16 +0100

    day 2 part 2

--- src/advent_of_code_clojure/2021/day_2.clj
+++ src/advent_of_code_clojure/2021/day_2.clj
@@ -18,10 +18,28 @@
       "up" [x (- y value)])))
 
 (defn compute-position [instructions]
-  (reduce adjust-pos [0 0] (input)))
+  (reduce adjust-pos [0 0] instructions))
 
 (comment 
   (let [pos (compute-position (input))]
     (* (first pos) (second pos))))
-
 ; Answer = 1746616
+
+
+; Part 2
+
+(defn adjust-pos-2 [position instruction]
+  (let [[x y aim] position
+        [direction value] instruction]
+    (case direction
+      "forward" [(+ x value) (+ y (* aim value)) aim]
+      "down" [x y (+ aim value)]
+      "up" [x y (- aim value)])))
+
+(defn compute-position-2 [instructions]
+  (reduce adjust-pos-2 [0 0 0] instructions))
+
+(comment
+  (let [pos (compute-position-2 (input))]
+    (* (first pos) (second pos))))
+; Answer = 1741971043
