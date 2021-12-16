448af1b91703d7f600a0a76eca0942b170f6fd76
Tormod Mathiesen <tormathi@gmail.com>
2021-12-16 09:25:47 +0100

    day 11 part 1

--- src/advent_of_code_clojure/2021/day_11.clj
+++ src/advent_of_code_clojure/2021/day_11.clj
@@ -1,9 +1,14 @@
 (ns advent-of-code-clojure.2021.day-11
-  (:require [advent-of-code-clojure.inputs :as inputs]))
+  (:require [advent-of-code-clojure.inputs :as inputs]
+            [clojure.string :as string]
+            [clojure.set :as cset]
+            [clojure.pprint :as pprint]))
 
 (defn parse-input [s]
-  (-> s
-      (inputs/lines)))
+  (->> s
+       (inputs/lines)
+       (mapv #(mapv read-string
+                    (string/split % #"")))))
 
 (def input
   (parse-input
@@ -22,3 +27,62 @@
 4846848554
 5283751526"))
 
+
+; Part 1
+
+
+(defn get-neighbours [m x y]
+  (let [all-possible (set (for [x (range (count (first m)))
+                                y (range (count m))]
+                            [x y]))
+        all-neighbours (set (for [x (range (dec x) (+ 2 x))
+                                  y (range (dec y) (+ 2 y))]
+                              [x y]))]
+    (cset/intersection all-possible all-neighbours)))
+
+(defn some-to-flash [m]
+  (let [flatm (flatten m)]
+    (when-let [n (some #(and (> % 9) %) flatm)]
+      (let [ind (.indexOf flatm n)
+            sizex (count (first m))]
+        [(mod ind sizex) (quot ind sizex)]))))
+
+(comment (->> test-input
+              (mapv #(mapv inc %))
+              (mapv #(mapv inc %))
+              (some-to-flash)))
+
+(defn flash! [m [x y]]
+  (let [neighbours (get-neighbours m x y)
+        inc-if-not-flashed #(if (not= 0 %) (inc %) %)]
+    (-> (reduce (fn [m [x y]]
+                  (update-in m [y x] inc-if-not-flashed))
+                m
+                neighbours)
+        (assoc-in [y x] 0))))
+
+(defn execute-flashes [m]
+  (loop [m m
+         nflashes 0]
+    (if-let [xy (some-to-flash m)]
+      (recur (flash! m xy) (inc nflashes))
+      [m nflashes])))
+
+(defn step [m]
+  (->> m
+       (mapv #(mapv inc %))
+       (execute-flashes)))
+
+(defn simulate [m steps]
+  (loop [m m 
+         nflashes 0
+         c 0]
+    (if (= c steps)
+      [m nflashes]
+      (let [[newm nflashed] (step m)] 
+        (recur newm (+ nflashes nflashed) (inc c))))))
+
+(comment
+  (pprint/pprint (simulate test-input 100))
+  (pprint/pprint (simulate input 100)))
+; Answer = 1588
