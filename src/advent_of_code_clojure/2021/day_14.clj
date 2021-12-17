c5743daefa2d6272b7bcbb92e3489e5f8acd0b28
Tormod Mathiesen <tormathi@gmail.com>
2021-12-17 14:07:56 +0100

    day 14 part 2

--- src/advent_of_code_clojure/2021/day_14.clj
+++ src/advent_of_code_clojure/2021/day_14.clj
@@ -69,8 +69,63 @@ CN -> C"))
   (let [[template insertions] test-input]
     (-> (apply-steps-ntimes template insertions 10)
         (high-minus-low)))
-  
+
   (let [[template insertions] input]
     (-> (apply-steps-ntimes template insertions 10)
         (high-minus-low))))
 ; Answer = 2360
+
+
+; Part 2
+
+(defn template->pairs [template]
+  (->> (drop-last template)
+       (map-indexed (fn [ind char] (str char (nth template (inc ind)))))
+       (frequencies)))
+
+(defn new-pairs
+  ([insertions pair]
+   (new-pairs insertions pair 1))
+  ([insertions pair occurances]
+   {(str (first pair) (insertions pair)) occurances
+    (str (insertions pair) (last pair)) occurances}))
+
+(defn step-2 [pairs insertions]
+  (->> pairs
+       (map (fn [[pair occurs]] (new-pairs insertions pair occurs)))
+       (apply (partial merge-with +))))
+
+
+
+(defn apply-steps-ntimes-2 [pairs insertions n]
+  (reduce (fn [acc _] (step-2 acc insertions))
+          pairs
+          (range n)))
+
+(comment
+  (let [[template insertions] test-input
+        pairs (template->pairs template)]
+    (= (apply-steps-ntimes-2 pairs insertions 10) 
+       (template->pairs (apply-steps-ntimes template insertions 10)))))
+
+
+(defn high-minus-low-2 [template pair-freqs]
+  (let [char-freqs (->> (map (fn [[k v]] {(first k) v}) pair-freqs)
+                        (apply (partial merge-with +))
+                        )
+        quantities (-> char-freqs 
+                       (update (last template) inc)
+                       (vals))]
+    (- (apply max quantities) (apply min quantities))))
+
+(comment
+  (let [[template insertions] test-input
+        pairs (template->pairs template)]
+    (->> (apply-steps-ntimes-2 pairs insertions 40)
+         (high-minus-low-2 template)))
+  
+  (let [[template insertions] input
+        pairs (template->pairs template)]
+    (->> (apply-steps-ntimes-2 pairs insertions 40)
+         (high-minus-low-2 template))))
+; Answer = 2967977072188
