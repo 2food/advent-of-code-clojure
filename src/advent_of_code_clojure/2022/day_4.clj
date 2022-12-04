041381494335a8d06ed0eb8edb4ac92c86e28a60
Tormod Mathiesen <tormathi@gmail.com>
2022-12-04 19:50:21 +0100

    2022 day 4

--- src/advent_of_code_clojure/2022/day_4.clj
+++ src/advent_of_code_clojure/2022/day_4.clj
@@ -1,21 +1,34 @@
 (ns advent-of-code-clojure.2022.day-4
-  (:require [advent-of-code-clojure.inputs :as inputs]))
+  (:require [advent-of-code-clojure.inputs :as inputs]
+            [clojure.string :as string]))
 
 (def input
   (->> (inputs/get-input-for-this-day)
-       ))
+       (inputs/lines)
+       (mapv (fn [line] (->> (string/split line #",")
+                             (mapv (fn [ranges] (mapv parse-long (string/split ranges #"-")))))))))
 
 ;; Part 1 
 
+(defn inside? [[a b] [x y]]
+  (and (>= a x) (<= b y)))
 
-(comment 
-  
+(defn fully-overlaps? [[a b]]
+  (or (inside? a b) (inside? b a)))
+
+(comment
+  (count (filter fully-overlaps? input))
+  ;; Answer = 556
   ) 
 
 ;; Part 2
 
+(defn somewhat-overlaps? [[[a b] [x y]]]
+  (or (<= x a y) (<= x b y)
+      (<= a x b) (<= a y b)))
 
-(comment 
-  
+(comment
+  (count (filter somewhat-overlaps? input))
+  ;; Answer = 876
   ) 
 
