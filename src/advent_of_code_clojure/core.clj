143e64c8ecfa93d85f53148c5b41a2989ae798b2
Tormod Mathiesen <tormathi@gmail.com>
2022-12-04 19:23:00 +0100

    generate files

--- src/advent_of_code_clojure/core.clj
+++ src/advent_of_code_clojure/core.clj
@@ -1,5 +1,37 @@
 (ns advent-of-code-clojure.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io])
+  (:import (java.time Year)))
+
+(defn new-year!
+  ([] (new-year! (.getValue (Year/now))))
+  ([year]
+   (io/make-parents (str "resources/inputs/" year "/whatever"))
+   (doseq [d (range 1 26)]
+     (let [f (str "src/advent_of_code_clojure/" year "/day_" d ".clj")]
+       (io/make-parents f)
+       (spit f
+             (str "(ns advent-of-code-clojure.2022.day-" d "
+  (:require [advent-of-code-clojure.inputs :as inputs]))
+
+(def input
+  (->> (inputs/get-input-for-this-day)
+       ))
+
+;; Part 1
+
+
+(comment \n  \n  )
+
+;; Part 2
+
+
+(comment \n  \n  )
+
+"))))))
+
+(comment
+  (new-year!))
 
 #_{:clj-kondo/ignore [:unused-binding]}
 (defn -main
