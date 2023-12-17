5476e3e487dbf5d06fff70270d143fc2c18ce6e9
Tormod Mathiesen <tormathi@gmail.com>
2023-12-17 20:07:45 +0100

    2023 day 9

--- src/advent_of_code_clojure/2023/day_9.clj
+++ src/advent_of_code_clojure/2023/day_9.clj
@@ -3,19 +3,60 @@
 
 (def input
   (->> (inputs/get-input-for-this-day)
-       ))
+       (inputs/lines-and-words)
+       (mapv #(mapv parse-long %))))
+
+(def example
+  (->> "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45"
+       (inputs/lines-and-words)
+       (mapv #(mapv parse-long %))))
 
 ;; Part 1
 
+(defn diffs [nums]
+  (mapv (fn [[a b]] (- b a)) (partition 2 1 nums)))
 
-(comment 
-  
+(defn all-diffs [nums]
+  (loop [nums        nums
+         known-diffs []]
+    (if (or (every? zero? nums) (empty? nums))
+      (conj known-diffs nums)
+      (let [new-nums (diffs nums)]
+        (recur new-nums (conj known-diffs nums))))))
+
+(defn extrapolate [nums]
+  (->> (all-diffs nums)
+       (map last)
+       (reverse)
+       (reduce + 0)))
+
+
+(comment
+
+  (->> (map extrapolate example)
+       (reduce +))
+
+  (->> (map extrapolate input)
+       (reduce +))
+
+  ; Answer = 1479011877
+
   )
 
 ;; Part 2
 
 
-(comment 
-  
+(comment
+
+  (->> (map reverse example)
+       (map extrapolate)
+       (reduce +))
+
+  (->> (map reverse input)
+       (map extrapolate)
+       (reduce +))
+
+  ; Answer = 973
+
   )
 
