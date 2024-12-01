1130ecfbe4852f12cccf7b826643a63bc2e7dc1c
Tormod Mathiesen <tormathi@gmail.com>
2024-12-01 22:11:31 +0100

    day 1

--- src/advent_of_code_clojure/2024/day_1.clj
+++ src/advent_of_code_clojure/2024/day_1.clj
@@ -3,18 +3,32 @@
 
 (def input
   (->> (inputs/get-input-for-this-day)
-       ))
+       (inputs/lines-and-words)
+       (flatten)
+       (keep parse-long)
+       (partition 2)
+       (mapv vec)))
 
 ;; Part 1
 
+(def left-list (sort (map first input)))
+(def right-list (sort (map second input)))
 
-(comment 
-  
+(comment
+  (->> (map - left-list right-list)
+       (map abs)
+       (reduce +))
   )
+
+; Answer = 1873376
+
 
 ;; Part 2
 
+(def right-freq (frequencies right-list))
 
-(comment 
-  
+(comment
+  (reduce + (map #(* % (get right-freq % 0)) left-list))
   )
+
+;; Answer = 18997088
