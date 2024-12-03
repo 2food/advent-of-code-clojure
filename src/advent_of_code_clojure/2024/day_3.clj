9e06f8fad5216e10aaff56612aed5154a48aed4c
Tormod Mathiesen <tormathi@gmail.com>
2024-12-03 13:30:07 +0100

    day 3

--- src/advent_of_code_clojure/2024/day_3.clj
+++ src/advent_of_code_clojure/2024/day_3.clj
@@ -2,20 +2,43 @@
   (:require [advent-of-code-clojure.inputs :as inputs]))
 
 (def input
-  (->> (inputs/get-input-for-this-day)
-       ))
+  (inputs/get-input-for-this-day))
 
 ;; Part 1
 
+(defn mult-and-sum [muls]
+  (->> muls
+       (map (fn [[n m]] (* n m)))
+       (reduce +)))
 
-(comment 
-  
+(comment
+  (->> (re-seq #"mul\((\d+),(\d+)\)" input)
+       (map (fn [[_ n m]] [(parse-long n) (parse-long m)]))
+       (mult-and-sum))
   )
+
+; Answer = 183788984
 
 ;; Part 2
 
 
-(comment 
-  
+(comment
+  (->> (re-seq #"do\(\)|don't\(\)|mul\((\d+),(\d+)\)" input)
+       (partition-by #(or (= (first %) "do()")
+                          (= (first %) "don't()")))
+       (reduce (fn [{:keys [do muls] :as acc} list]
+                 (case (ffirst list)
+                   nil acc
+                   "do()" (assoc acc :do true)
+                   "don't()" (assoc acc :do false)
+                   (if do
+                     (assoc acc :muls (concat muls list))
+                     acc)))
+               {:do   true
+                :muls []})
+       :muls
+       (map (fn [[_ n m]] [(parse-long n) (parse-long m)]))
+       (mult-and-sum))
   )
 
+;; Answer = 62098619
