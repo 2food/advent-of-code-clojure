cf44d1b172cd0d884f6633fd418a00b8f2cd5606
Tormod Mathiesen <tormathi@gmail.com>
2025-12-01 19:13:57 +0100

    new year! 2025

---
+++ src/advent_of_code_clojure/2025/day_6.clj
@@ -0,0 +1,21 @@
+(ns advent-of-code-clojure.2025.day-6
+  (:require [advent-of-code-clojure.inputs :as inputs]))
+
+(def input
+  (->> (inputs/get-input-for-this-day)
+       ))
+
+;; Part 1
+
+
+(comment 
+  
+  )
+
+;; Part 2
+
+
+(comment 
+  
+  )
+
