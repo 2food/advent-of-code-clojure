666b133a5343ba0aecfc8452b3d789ef628dc38e
Tormod Mathiesen <tormathi@gmail.com>
2025-12-05 09:34:33 +0100

    2025, day 5

--- src/advent_of_code_clojure/2025/day_5.clj
+++ src/advent_of_code_clojure/2025/day_5.clj
@@ -1,21 +1,89 @@
 (ns advent-of-code-clojure.2025.day-5
-  (:require [advent-of-code-clojure.inputs :as inputs]))
+  (:require [advent-of-code-clojure.inputs :as inputs]
+            [clojure.string :as str]))
+
+(defn parse-input [s]
+  (let [lines (inputs/lines s)]
+    {:ranges (mapv (fn [s] (mapv parse-long (str/split s #"-"))) (take-while (complement str/blank?) lines))
+     :ids    (mapv parse-long (rest (drop-while (complement str/blank?) lines)))}))
+
+(def example-input
+  (->> ["3-5"
+        "10-14"
+        "16-20"
+        "12-18"
+        ""
+        "1"
+        "5"
+        "8"
+        "11"
+        "17"
+        "32"]
+       (str/join "\n")
+       (parse-input)))
 
 (def input
   (->> (inputs/get-input-for-this-day)
-       ))
+       (parse-input)))
 
 ;; Part 1
 
+(defn in-range? [[a b] x]
+  (<= a x b))
 
-(comment 
-  
+(defn in-any-range? [ranges x]
+  (some #(in-range? % x) ranges))
+
+(comment
+
+  (let [{:keys [ranges ids]} example-input]
+    (count (filter (partial in-any-range? ranges) ids)))
+
+  (let [{:keys [ranges ids]} input]
+    (count (filter (partial in-any-range? ranges) ids)))
+
+  ; Answer: 798
+
   )
 
 ;; Part 2
 
+(defn overlaps? [[x1 y1] [x2 y2]]
+  (or (<= x1 x2 y1) (<= x1 x2 y1)
+      (<= x2 x1 y2) (<= x2 y1 y2)))
 
-(comment 
-  
+(defn union [[x1 y1] [x2 y2]]
+  (if (overlaps? [x1 y1] [x2 y2])
+    [(min x1 x2) (max y1 y2)]
+    [[x1 y1] [x2 y2]]))
+
+(defn unionize-all-ranges [ranges]
+  (loop [ranges ranges]
+    (if-let [overlapping (some (fn [r] (some #(and (not= r %)
+                                                   (overlaps? r %)
+                                                   [r %])
+                                             ranges))
+                               ranges)]
+      (let [[r1 r2] overlapping
+            new-r (union r1 r2)]
+        (recur (conj (set (remove #{r1 r2} ranges)) new-r)))
+      ranges)))
+
+(comment
+
+  (let [{:keys [ranges]} example-input]
+    (->> ranges
+         (unionize-all-ranges)
+         (map (fn [[x y]] (inc (- y x))))
+         (reduce +)))
+
+  (let [{:keys [ranges]} input]
+    (->> ranges
+         (unionize-all-ranges)
+         (map (fn [[x y]] (inc (- y x))))
+         (reduce +)))
+
+  ; Answer: 366181852921027
+
   )
 
--- src/advent_of_code_clojure/2025/day_6.clj
+++ src/advent_of_code_clojure/2025/day_6.clj
@@ -1,21 +1,86 @@
 (ns advent-of-code-clojure.2025.day-6
-  (:require [advent-of-code-clojure.inputs :as inputs]))
+  (:require [advent-of-code-clojure.inputs :as inputs]
+            [advent-of-code-clojure.utils :as utils]
+            [clojure.string :as str]))
+
+(def example-input
+  (inputs/lines "123 328  51 64 \n 45 64  387 23 \n  6 98  215 314\n*   +   *   +  "))
 
 (def input
-  (->> (inputs/get-input-for-this-day)
-       ))
+  (inputs/lines (inputs/get-input-for-this-day)))
 
 ;; Part 1
 
 
-(comment 
-  
+(defn parse-word [s]
+  (cond
+    (= "*" s) :*
+    (= "+" s) :+
+    :else (parse-long s)))
+
+(defn parse-input [s]
+  (->> s
+       (mapv inputs/words)
+       (mapv #(vec (keep parse-word %)))))
+
+(defn calc [& args]
+  (let [nums (drop-last args)
+        op (last args)]
+    (case op
+      :* (apply * nums)
+      :+ (apply + nums))))
+
+(comment
+
+  (->> (parse-input example-input)
+       (apply map calc)
+       (reduce +))
+
+  (->> (parse-input input)
+       (apply map calc)
+       (reduce +))
+
+  ; Answer = 4583860641327
+
   )
 
 ;; Part 2
 
+(defn number-groups [input]
+  (->> input
+       (drop-last)
+       (utils/transpose)
+       (map (comp parse-long str/trim str/join))
+       (partition-by some?)
+       (remove (comp nil? first))
+       (mapv vec)))
 
-(comment 
-  
+(defn ops [input]
+  (->> (last input)
+       (inputs/words)
+       (keep parse-word)))
+
+
+(comment
+  (let [inp example-input]
+    (->> (map (fn [nums op]
+                (case op
+                  :* (apply * nums)
+                  :+ (apply + nums)))
+              (number-groups inp)
+              (ops inp))
+         (reduce +)))
+
+  (let [inp input]
+    (->> (map (fn [nums op]
+                (case op
+                  :* (apply * nums)
+                  :+ (apply + nums)))
+              (number-groups inp)
+              (ops inp))
+         (reduce +)))
+
+  ; Answer = 11602774058280
+
   )
 
